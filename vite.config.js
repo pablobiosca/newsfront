5fe8f7278170b36ea508b1f558be4f03a0e6ff94
pablobiosca <zaslake@gmail.com>
2025-02-01 10:04:23 +0100

    feat(config): add alias path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // Asegúrate de que apunte a la carpeta 'src'
+    }
+  }
 })

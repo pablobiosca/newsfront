5e22085b9154b2b910842e9fc6ab598a2c587e22
pablobiosca <zaslake@gmail.com>
2025-02-04 18:48:50 +0100

    feaat(createnew):add default image to backend new item

--- src/hooks/usePopup.js
+++ src/hooks/usePopup.js
@@ -21,8 +21,14 @@ export const useAddNewForm = (closePopup,refreshNews) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     console.log(formData)
-    const newNew = await createNew(formData)
+
+    const newsData = {
+        ...formData,
+        imageUrl: formData.imageUrl || "https://www.dzoom.org.es/wp-content/uploads/2013/04/manzana-equilibrio-734x486.jpg",
+    }
     
+    const newNew = await createNew(newsData)
+
     console.log("verificando data desde handlesubmit =>",newNew)
     refreshNews(newNew)
     closePopup()

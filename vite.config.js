8b1892ccd8fd6ba856f36600caba388abb81d497
pablobiosca <zaslake@gmail.com>
2025-02-04 17:00:01 +0100

    feat(config): edit config files,alias & pathsf routes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // Asegúrate de que apunte a la carpeta 'src'
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
